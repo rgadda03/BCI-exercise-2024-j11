a90227491ff5c5fcedb1aaa83492740b734a5ae5
Rodrigo Gadda <rodrigo.gadda@globallogic.com>
2024-07-30 12:29:55 -0300

    start spring io

---
+++ src/main/java/com/demoj11/demoj11/Demoj11Application.java
@@ -0,0 +1,13 @@
+package com.demoj11.demoj11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demoj11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demoj11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/demoj11/demoj11/Demoj11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoj11.demoj11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demoj11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
