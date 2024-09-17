1059b5a2358cc374eca1b0f33a380a9c0454b64d
Rodrigo Gadda <rodrigo.gadda@globallogic.com>
2024-09-17 14:10:22 -0300

    se limpia todo lo de maven

--- src/main/java/com/demoj11/demoj11/exception/SuperErrorException.java
+++
@@ -1,9 +0,0 @@
-package com.demoj11.demoj11.exception;
-
-import java.io.Serializable;
-
-public abstract class  SuperErrorException extends Throwable implements Serializable{
-
-	private static final long serialVersionUID = 1L;
-
-}
